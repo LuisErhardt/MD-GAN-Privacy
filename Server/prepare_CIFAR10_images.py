462de7ce9d52b16218cd90474a078fa036b4851a
Luis <lerhardt@rhrk.uni-kl.de>
2023-09-05 11:35:55 +0200

    create imgs dir

--- Server/prepare_CIFAR10_images.py
+++ Server/prepare_CIFAR10_images.py
@@ -1,9 +1,12 @@
 from torchvision import datasets
 from torchvision import transforms
+from pathlib import Path
 from torchvision.utils import save_image
-transform = transforms.Compose([transforms.Resize(32),transforms.ToTensor(),transforms.Normalize(mean=[0.5], std=[0.5])])
-dataset = datasets.CIFAR10(root='./data', train=True, download=False, transform=transform)
 
+transform = transforms.Compose([transforms.Resize(32),transforms.ToTensor(),transforms.Normalize(mean=[0.5], std=[0.5])])
+dataset = datasets.CIFAR10(root='data/cifar10', train=True, download=False, transform=transform)
+
+Path("data/imgs").mkdir(parents=False, exist_ok=True)
 
 for idx, (img, _) in enumerate(dataset):
     save_image(img, 'data/imgs/{:05d}.jpg'.format(idx))
